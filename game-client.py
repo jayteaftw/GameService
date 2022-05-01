343435329a7ae6cc5eac1524a3558ae4921860e0
JT <jtvinolus@gmail.com>
2022-05-01 14:58:08 -0700

    Add packet send and recieve for client and server

--- game-client.py
+++ game-client.py
@@ -4,60 +4,139 @@ import pygame
 import numpy as np  
 from io import BytesIO     
 import pickle 
-import time   
+import time
+import sys
+from struct import unpack   
  
-# Create a socket object
-s = socket.socket()        
- 
-# Define the port on which you want to connect
-port = 12356              
- 
-# connect to the server on local computer
-s.connect(('127.0.0.1', port))
- 
-# receive data from the server and decoding to get the string.
-print (s.recv(1024).decode())
+def streaming_game(size):
+    # Create a socket object
+    s = socket.socket()        
+
+    # Define the port on which you want to connect
+    port = 12356              
+
+    # connect to the server on local computer
+    s.connect(('127.0.0.1', port))
+
+    # receive data from the server and decoding to get the string.
+    print (s.recv(1024).decode())
+    count = 0
+    running = True
+    events_to_send = " "
+    while running:
+        
+        #time.sleep(.1)
+        
+        def receive_data(conn):
+            data_size = unpack('>I', conn.recv(4))[0]
+            received_payload = b""
+            reamining_payload_size = data_size
+            while reamining_payload_size != 0:
+                received_payload += conn.recv(reamining_payload_size)
+                reamining_payload_size = data_size - len(received_payload)
+            data = pickle.loads(received_payload)
+
+            return data
+        
+        recieved = receive_data(s)
+
+        s.send(events_to_send.encode())
+        if events_to_send == "STOP":
+            s.close()  
+            return
+        
+        stream = pickle.loads(recieved)
+
+        def command_conversion(command):
+            if command == pygame.K_ESCAPE:  return "STOP"
+            if command == pygame.K_a:       return "A"
+            if command == pygame.K_b:       return "B"  
+            if command == pygame.K_UP:      return "UP"
+            if command == pygame.K_DOWN:    return "DOWN"
+            if command == pygame.K_LEFT:    return "LEFT"
+            if command == pygame.K_RIGHT:   return "RIGHT"
+
+        events_to_send = " "
+        for event in pygame.event.get():
+            if event.type == pygame.VIDEORESIZE:
+                size = (event.w, event.h)
+            if event.type == pygame.KEYDOWN:
+                events_to_send = command_conversion(event.key)
+                print(f"You pressed {events_to_send}")
+            if event.type == pygame.KEYUP:
+                events_to_send = "!"+command_conversion(event.key)
+                print(f"You Released {events_to_send}")
+
+                
+                
+
+        org_pic = pygame.surfarray.make_surface(stream)
+        pic = pygame.transform.scale(org_pic, size)
+        
+        BLACK = (0, 0, 0)
+        screen.fill(BLACK)
+        
+        screen.blit(pic, (0,0))
+        
+        #pygame.display.flip()
+        pygame.display.update()
+
+def draw_text(text, font, color, surface, x, y):
+    textobj = font.render(text, 1, color)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x, y)
+    surface.blit(textobj, textrect)
+
+def main_menu(mainClock, size):
+
+    click = False
+    while True:
+
+        screen.fill((0,0,0))
+        draw_text('main menu', font, (255, 255, 255), screen, 20, 20)
+
+        mx, my = pygame.mouse.get_pos()
+
+        button_1 = pygame.Rect(50, 100, 200, 50)
+        button_2 = pygame.Rect(50, 200, 200, 50)
+        if button_1.collidepoint((mx, my)):
+            if click:
+                streaming_game(size)
+        if button_2.collidepoint((mx, my)):
+            if click:
+                pass
+        pygame.draw.rect(screen, (255, 0, 0), button_1)
+        pygame.draw.rect(screen, (255, 0, 0), button_2)
+
+        click = False
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    click = True
+
+        pygame.display.update()
+        mainClock.tick(60)
 
 
-pygame.init()
-FACTOR = 5
-size = (160*FACTOR, 144*FACTOR)
-screen = pygame.display.set_mode(size, pygame.RESIZABLE)
-
-count = 0
-while True:
-    time.sleep(0.01)
-    recieved = s.recv(69283)
-    length = s.recv(100)
-    if str(length) != f"b'{str(len(recieved))}'":
-        #print(length, len(recieved) )
-        count += 1
-        print("Drop Count:",count)
-        continue
+if __name__ == "__main__":
     
-    stream = pickle.loads(recieved)
-    #print("recieved:",recieved)
 
-    #print(stream.shape, type(stream))
-    #print("okay")
-
-    for event in pygame.event.get():
-        if event.type == pygame.VIDEORESIZE:
-            size = (event.w, event.h)
-
-    org_pic = pygame.surfarray.make_surface(stream)
-    pic = pygame.transform.scale(org_pic, size)
-
-    #print("stream:",stream.shape, "\n screen1", screen.get_size())
     
-    BLACK = (0, 0, 0)
-    screen.fill(BLACK)
-    
-    screen.blit(pic, (0,0))
-    
-    #pygame.display.flip()
-    pygame.display.update()
+    main_clock = pygame.time.Clock()
+    pygame.init()
+    FACTOR = 5
+    size = (160*FACTOR, 144*FACTOR)
+    pygame.display.set_caption('Pokemon')
+    screen = pygame.display.set_mode(size, pygame.RESIZABLE)
 
+    font = pygame.font.SysFont(None, 20)
+    main_menu(main_clock, size)
+    #streaming_game(size)
 
-# close the connection
-s.close()  
--- game-server.py
+++ game-server.py
@@ -5,74 +5,88 @@ from pyboy import PyBoy
 from pyboy import WindowEvent
 from io import BytesIO
 import pickle
+import time
+from struct import pack
  
-# next create a socket object
-s = socket.socket()        
-print ("Socket successfully created")
- 
-# reserve a port on your computer in our
-# case it is 12345 but it can be anything
-port = 12356              
- 
-# Next bind to the port
-# we have not typed any ip in the ip field
-# instead we have inputted an empty string
-# this makes the server listen to requests
-# coming from other computers on the network
-s.bind(('', port))        
-print ("socket binded to %s" %(port))
- 
-# put the socket into listening mode
-s.listen(5)    
-print ("socket is listening")           
- 
-# a forever loop until we interrupt it or
-# an error occurs
-while True:
- 
-    # Establish connection with client.
-    c, addr = s.accept()    
-    print ('Got connection from', addr )
- 
-    # send a thank you message to the client. encoding to send byte type.
-    c.send('Starting Game Service'.encode())
 
+if __name__ == "__main__":
+    s = socket.socket()        
+    print ("Socket successfully created")
     
+    port = 12356              
+    
+    s.bind(('', port))        
+    print ("socket binded to %s" %(port))
+    
+    s.listen(5)    
+    print ("socket is listening")           
+    
+    while True:
+        
+        # Establish connection with client.
+        c, addr = s.accept()    
+        print ('Got connection from', addr )
+    
+        # send a thank you message to the client. encoding to send byte type.
+        c.send('Starting Game Service'.encode())
 
-    pyboy = PyBoy('ROMs/Pokemon Red (UE) [S][!].gb')
-    pyboy.set_emulation_speed(1)
-    bot = pyboy.botsupport_manager()
-    screen_bot = bot.screen()
+        pyboy = PyBoy('ROMs/Pokemon Red (UE) [S][!].gb')
+        pyboy.set_emulation_speed(5)
+        bot = pyboy.botsupport_manager()
+        screen_bot = bot.screen()
 
-    while not pyboy.tick():
+        button_press, button_release = None, None
+        while not pyboy.tick():
+            #time.sleep(0.001)
 
-        stream = np.transpose(screen_bot.screen_ndarray(), (1,0,2))
+            stream = np.transpose(screen_bot.screen_ndarray(), (1,0,2))
 
-        package = pickle.dumps(stream)
-        print(type(package), len(package))
-        c.send(package)
-        c.send(str(len(package)).encode())
+            package = pickle.dumps(stream)
+            #print(type(package), len(package))
+            
+            def send_data(conn, data):
+                serialized_data = pickle.dumps(data)
+                conn.sendall(pack('>I', len(serialized_data)))
+                conn.sendall(serialized_data)
+            
+            send_data(c, package)
+
+            command = c.recv(10).decode()
+
+            def command_conversion(command):
+                if command == "A":      return WindowEvent.PRESS_BUTTON_A      
+                if command == "!A":     return WindowEvent.RELEASE_BUTTON_A
+
+                if command == "B":      return WindowEvent.PRESS_BUTTON_A      
+                if command == "!B":     return WindowEvent.RELEASE_BUTTON_A
+                
+                if command == "UP":     return WindowEvent.PRESS_ARROW_UP
+                if command == "!UP":    return WindowEvent.RELEASE_ARROW_UP
+                
+                if command == "DOWN":   return WindowEvent.PRESS_ARROW_DOWN    
+                if command == "!DOWN":  return WindowEvent.RELEASE_ARROW_DOWN
+                
+                if command == "RIGHT":  return WindowEvent.PRESS_ARROW_RIGHT
+                if command == "!RIGHT": return WindowEvent.RELEASE_ARROW_RIGHT
+                
+                if command == "LEFT":   return WindowEvent.PRESS_ARROW_LEFT
+                if command == "!LEFT":  return WindowEvent.RELEASE_ARROW_LEFT
+
+
+            if command == "STOP":
+                c.close()
+                exit()
+            if command != " ":
+                print(command)
+                command = command_conversion(command)
+                pyboy.send_input(command)
+                pyboy.tick()
+           
+            
         
 
-        """  #message = c.recv(1024).decode()
-        message = c.recv(1024)
-        print(message, type(message))
-
-        # Close the connection with the client
-        if message == "close":
-            print("Closing Server")
-            break """
-
-        pass
     
 
-    message = c.recv(1024).decode()
-    print(message, type(message))
+    #c.close()
 
-    c.close()
-
-    # Close the connection with the client
-    if message == "close":
-        print("Closing Server")
-        break
         
